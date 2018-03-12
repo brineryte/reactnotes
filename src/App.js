820eda0e64a7408cd022be172b6b62d9efd00af7
Brian Wright <bwright@webstaurantstore.com>
2018-03-12 00:03:53 -0400

    Initial commit. Almost finished with application.

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './App.css';
+import Note from './Note/Note';
+import NoteForm from './NoteForm/NoteForm'
+
+class App extends Component {
+  
+  constructor(props){
+    super(props);
+    this.addNote = this.addNote.bind(this);
+    
+    this.state = {
+      notes: [
+        {id:1,noteContent:"Note 1 here!"},
+        {id:2,noteContent:"Note 2 here!"},
+      ]
+    }
+  }
+
+  addNote(note){
+    // push the note onto the notes array
+    const previousNotes = this.state.notes;
+    previousNotes.push(note);
+    this.setState({
+      notes:  previousNotes
+    }); 
+  }
+  
+  render() {
+    return (
+      <div className="notesWrapper">
+        <div className="notesHeader">
+          <div className="heading">To-Do List</div>
+        </div>
+        <div className="notesBody">
+          {
+            this.state.notes.map((note) => {
+              return (
+                <Note noteContent={note.noteContent} noteId={note.id} key={note.id} />
+              )
+            })
+          }
+        </div>
+        <div className="notesFooter">
+          <NoteForm addNote={this.addNote}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
